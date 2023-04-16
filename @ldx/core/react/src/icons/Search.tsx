713312013270598752061aeb37a69749741304c9
Marius Rostad <marostad98@gmail.com>
2023-04-16 10:53:33 +0200

    bug: React style properties

--- @ldx/core/react/src/icons/Search.tsx
+++ @ldx/core/react/src/icons/Search.tsx
@@ -16,7 +16,7 @@ export default function SearchIcon(props: SearchIconProps) {
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
         aria-hidden="true"
         focusable="false"
         aria-labelledby="menuSearch"
