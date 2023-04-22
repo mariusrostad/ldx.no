a30553c862f0df557e685654aa4c6bdd30014beb
Marius Rostad <marostad98@gmail.com>
2023-04-22 14:21:48 +0200

    feature: Center login button

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -3,8 +3,14 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
   return (
-    <main>
+    <div
+      style={{
+        maxWidth: "1024px",
+        margin: "auto",
+        padding: "32px",
+      }}
+    >
       <button onClick={() => signIn("github")}>Login</button>
-    </main>
+    </div>
   );
 }
