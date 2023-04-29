356a50ccb03fe1b70e1914a577e01074e35b5c5b
Marius Rostad <marostad98@gmail.com>
2023-04-29 23:25:32 +0200

    feature: front page services cards

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -8,8 +8,24 @@ export default function Home() {
           padding: "32px",
         }}
       >
-        <h1 className="primary-color">Velkommen til Marius Rostad.no</h1>
+        <h1 className="primary-color">Velkommen til ldx.no</h1>
         <p>Sjekk ut våre tjenester</p>
+        <div className="card-container">
+          <div className="card">
+            <h2>Designsystem</h2>
+            <p>
+              Den visuelle representasjonen av ldx.no / mariusrostad.no /
+              mariusrostad.com
+            </p>
+          </div>
+          <div className="card">
+            <h2>Ressursoversikt</h2>
+            <p>
+              Enkelt system for å logge konsulent oppdrag / prosjekter for alle
+              ansatte
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
