9cb2ebb94bdfd86219bd2438aa032843b3083fe5
Marius Rostad <marostad98@gmail.com>
2023-05-01 12:47:54 +0200

    feature: Fjernet mariusrostad.no fra ekstempel tekstene

--- apps/web/src/app/ds/page.tsx
+++ apps/web/src/app/ds/page.tsx
@@ -1,6 +1,7 @@
 export const metadata = {
-  title: "Designsystem ldx.no",
-  description: "Designsystem nettside for ldx.no",
+  title: "Designsystemet til ldx.no",
+  description:
+    "Et design system er et sett med komponenter og visuellet stiler som leverer den visuelle profilen til nettstedet.",
 };
 
 export default function DesignSystem() {
@@ -12,14 +13,15 @@ export default function DesignSystem() {
         margin: "auto",
       }}
     >
-      <h1 className="primary-color">H1 velkommen til MariusRostad.no</h1>
-      <h2 className="primary-color">H2 velkommen til MariusRostad.no</h2>
-      <h3 className="primary-color">H3 velkommen til MariusRostad.no</h3>
-      <h4 className="primary-color">H4 velkommen til MariusRostad.no</h4>
-      <h5 className="primary-color">H5 velkommen til MariusRostad.no</h5>
-      <h6 className="primary-color">H6 velkommen til MariusRostad.no</h6>
-      <a href="/ds">Anchor tag</a>
-      <p>Paragraph</p>
+      <h1 className="primary-color">H1 med en fin og tekst</h1>
+      <h2 className="primary-color">H2 med en fin og tekst</h2>
+      <h3 className="primary-color">H3 med en fin og tekst</h3>
+      <h4 className="primary-color">H4 med en fin og tekst</h4>
+      <h5 className="primary-color">H5 med en fin og tekst</h5>
+      <h6 className="primary-color">H6 med en fin og tekst</h6>
+      <a href="/ds">Link html tag</a>
+      <p>Paragraph tekst</p>
+      <b>Bold tekst</b>
     </main>
   );
 }
